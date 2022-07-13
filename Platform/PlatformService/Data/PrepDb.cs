31a80d3ef3ce8a0066541f315cf17a30d9460f7c
Talha Abbasi <talhaabbasi2009@hotmail.com>
2022-07-13 08:09:20 +0500

    Initial Commit

--- Platform/PlatformService/Data/PrepDb.cs
+++ Platform/PlatformService/Data/PrepDb.cs
@@ -1,7 +1,20 @@
-﻿namespace PlatformService.Data
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace PlatformService.Data
 {
     public static class PrepDb
     {
+        public static void PrepPopulation(IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices.CreateScope())
+            {
+                SeedData(serviceScope.ServiceProvider.GetService<AppDbContext>());
+            }
+        }
 
+        private static void SeedData(AppDbContext context)
+        {
+            if(!context.Platforms.Add)
+        }
     }
 }
