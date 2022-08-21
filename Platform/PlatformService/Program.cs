de84f9f946bfb3c0234c27e681ef4d0256ab90ba
Talha Abbasi <talhaabbasi2009@hotmail.com>
2022-08-21 15:54:38 +0500

    [FEAT] Add AutoMapper 💪

--- Platform/PlatformService/Program.cs
+++ Platform/PlatformService/Program.cs
@@ -9,6 +9,7 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
     opt.UseInMemoryDatabase("InMem"));
 builder.Services.AddScoped<IPlatformRepo, PlatformRepo>();
 builder.Services.AddControllers();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
